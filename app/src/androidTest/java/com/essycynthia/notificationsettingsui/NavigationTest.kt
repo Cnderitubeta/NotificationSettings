965eb4781fc04c632f001e3f0dd5fe9fc52f7ebf
Cnderitubeta <essyc14@gmail.com>
2023-08-09 23:52:38 +0300

    More tests added

--- app/src/androidTest/java/com/essycynthia/notificationsettingsui/NavigationTest.kt
+++ app/src/androidTest/java/com/essycynthia/notificationsettingsui/NavigationTest.kt
@@ -4,8 +4,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -13,21 +15,38 @@ import org.junit.Test
 class NavigationTest {
     @get:Rule
     val composeTestRule = createComposeRule()
-    lateinit var navController : TestNavHostController
+    lateinit var navController: TestNavHostController
+
     @Before
-    fun setupNavHost(){
+    fun setupNavHost() {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
             navController.navigatorProvider.addNavigator(ComposeNavigator())
             BottomNavGraph(navContoller = navController)
         }
     }
+
     @Test
 
-    fun verify_startDestinationIsAppMainScreen(){
+    fun verify_startDestinationIsAppMainScreen() {
         composeTestRule.onNodeWithText("Welcome to Software Update Notifications")
             .assertIsDisplayed()
     }
 
+    @Test
+    fun isBottomNavigationDisplayed() {
+        composeTestRule.onNodeWithText("Home")
+            .assertIsDisplayed()
 
+
+    }
+    @Test
+    fun ensureNavigationIsWorkingAsExpected(){
+
+       composeTestRule. onNodeWithText("Home")
+            .assertIsDisplayed()
+           .performClick()
+        val route = navController.currentBackStackEntry?.destination?.route
+        Assert.assertEquals(route,"main_screen")
+    }
 }
